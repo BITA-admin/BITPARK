2f190a2c0ecd6c39bfa950735ade0d02a7224fcd
arun-vijay-vk <vijayakuamrarun07@gmail.com>
2024-06-03 11:01:24 +0530

    conban

--- src/Components/Contact_Us/Contact_Banner.jsx
+++ src/Components/Contact_Us/Contact_Banner.jsx
@@ -1,28 +1,6 @@
-<<<<<<< HEAD
-import Contact_Us from "../../assets/all-images/email-6370595_1280.png";
-import Contact_us from "../../assets/all-images/Contact_us_12.png";
-
-const Sample = () => {
-  return (
-    <div>
-      <div className="relative flex h-screen">
-        <img
-          src={Contact_Us}
-          alt="bitpark-Contact_us"
-          className="h-full w-full object-cover object-center"
-        />
-        <div className="absolute top-1/2 left-20 max-w-full md:left-44 z-10">
-          <div className=" text-transparent bg-clip-text bg-gradient-to-r from-teal-500 via-sky-400 to-cyan-500 font-extrabold xl:text-4xl lg:text-3xl tracking-wider py-44 lg:py-24  xl:py-4 text-3xl">
-            <span className="block w-full">Reach out for expert</span>
-            software development <br />
-            and IT consulting
-=======
-import Contact_Us from "../../assets/all-images/email-6370595_1280.jpg";
+// import Contact_Us from "../../assets/all-images/email-6370595_1280.png";
 // import Contact_us from "../../assets/all-images/Contact_us_12.png";
 import Contact_us from "../../assets/all-images/contactpagebannerimg2.png";
-
-
-
 
 const Sample = () => {
   return (
@@ -42,13 +20,12 @@ const Sample = () => {
             </div>
           </div>
           <div className="absolute lg:top-[5%] max-w-[40%] top-32 md:py-10 md:top-10 lg:right-0  z-10 ">
->>>>>>> a082818082e04e62306824d2001bed0cc291debc
           </div>
         </div>
         <div className="h-full w-full bg-slate-900 absolute top-0 opacity-75 "></div>
       </div> */}
 
-      <section className=" bg-[url('src/assets/all-images/email-6370595_1280.jpg')] bg-no-repeat bg-cover">
+      <section className=" bg-[url('src/assets/all-images/email-6370595_1280.png')] bg-no-repeat bg-cover">
         <div className=" bg-slate-900/75 pt-[25%] lg:pt-[8%] flex flex-col lg:flex-row items-center justify-around">
           <div>
             <div className=" text-transparent bg-clip-text bg-gradient-to-r from-teal-500 via-sky-400 to-cyan-500 font-extrabold xl:text-4xl lg:text-3xl tracking-wider text-3xl">
@@ -57,7 +34,11 @@ const Sample = () => {
               and IT consulting
             </div>
           </div>
-          <img src={Contact_us} alt="Contact_Banner" className="w-[85%] pt-[8%] lg:pt-0 lg:w-[45%]" />
+          <img
+            src={Contact_us}
+            alt="Contact_Banner"
+            className="w-[85%] pt-[8%] lg:pt-0 lg:w-[45%]"
+          />
         </div>
       </section>
     </>
